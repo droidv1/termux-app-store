9c22c1908b8df3a2d99d9ae003a2380758c41efb
djunekz <gab288.gab288@passinbox.com>
2026-02-03 08:57:16 +0700

    New package: webshake scanning vulnerabillity and analyst web

---
+++ packages/webshake/build.sh
@@ -0,0 +1,9 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/djunekz/webshake
+TERMUX_PKG_DESCRIPTION="Scan vulnerabillity and analyst website"
+TERMUX_PKG_LICENSE="PROPRIETARY"
+TERMUX_PKG_MAINTAINER="@djunekz <gab288.gab288@gmail.com>"
+TERMUX_PKG_VERSION=1.0.2
+TERMUX_PKG_SRCURL=https://github.com/djunekz/webshake/releases/download/v${TERMUX_PKG_VERSION}/webshake-${TERMUX_PKG_VERSION}.tar.gz
+TERMUX_PKG_SHA256=4312838c7d201f1540923dfbeeeb21d8af4ae9896a2ce18357208805a859cbe8
+#TERMUX_PKG_DEPENDS=""
+#TERMUX_PKG_BUILD_IN_SRC=true
