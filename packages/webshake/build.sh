91b68668202dcda4a187e1a6fc7ab53cfa22c3d5
djunekz <gab288.gab288@passinbox.com>
2026-02-10 18:51:36 +0700

    update webshake build

--- packages/webshake/build.sh
+++ packages/webshake/build.sh
@@ -1,7 +1,7 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/djunekz/webshake
-TERMUX_PKG_DESCRIPTION="Scan vulnerabillity and analyst website"
-TERMUX_PKG_LICENSE="PROPRIETARY"
-TERMUX_PKG_MAINTAINER="@djunekz"
+TERMUX_PKG_DESCRIPTION="Scan vulnerabilities and analyze websites"
+TERMUX_PKG_LICENSE="Custom"
+TERMUX_PKG_MAINTAINER="Djunekz"
 TERMUX_PKG_VERSION=1.0.2
 TERMUX_PKG_SRCURL=https://github.com/djunekz/webshake/releases/download/v${TERMUX_PKG_VERSION}/webshake-${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_SHA256=9998fe24538edb31da24175e889a11b57e7e62f9effa98ffa24cd707675279e7
